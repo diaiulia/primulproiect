94658e5aeeb93c403ff267628159653cf308abe6
Diana <dianasin12@yahoo.com>
2024-03-07 21:36:55 +0200

    lectia 1

---
+++ src/learning/ClasaNoua.java
@@ -0,0 +1,32 @@
+package learning;
+
+public class ClasaNoua {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello Skillbrain");
+
+        int numar;
+        numar = 5;
+
+        int numar2 = 67567;
+        Integer numarClasa = 10;
+
+
+        float numarCuVirgula = 7.3f;
+        double numarCuPunct = 8.5;
+
+        char unSingurCaracter = 'G';
+        long numarFoarteMare = 1264433568699L;
+
+        short numarMaiMic = (short)  2354687;
+        boolean trueSauFalse = true;
+        boolean trueSauFalse2 = false;
+
+        byte valoareMax = -128;
+
+
+
+
+    }
+}
